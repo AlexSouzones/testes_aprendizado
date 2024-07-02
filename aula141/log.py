e70cb0424d220e616c4f54ec235ca000b7e4ddae
AlexSouzones <alexandresouzones@gmail.com>
2024-07-02 04:28:40 -0300

    aula 142

--- aula141/log.py
+++ aula141/log.py
@@ -1,13 +1,27 @@
 class Log:
-  def log(self, msg):
+  def _log(self, msg):
     raise NotImplementedError("Implemente o metodo log")
   
 
+  def log_error(self, msg):
+    return self._log(f"Error: {msg}")
+  
+
+  def log_sucess(self, msg):
+    return self._log(f"Sucess: {msg}")
+  
+
 class LogFileMixin(Log):
-  def log(self, msg):
-    print(msg) 
+  def _log(self, msg):
+    print(msg)
+
+
+class LogPrintMixin(Log):
+  def _log(self, msg):
+    print(f"{msg} {self.__class__.__name__}") 
   
 
 if __name__ == "__main__":
-  i = LogFileMixin()
-  i.log("Mensagem")
+  i = LogPrintMixin()
+  i.log_error("Mensagem")
+  i.log_sucess("Que legal")
